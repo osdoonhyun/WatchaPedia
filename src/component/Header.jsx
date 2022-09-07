fb4e9c953171f2a733783143abe323e6409d4862
osdoonhyun <skw1996@naver.com>
2022-09-08 02:54:36 +0900

    Fix Refresh page F5 error

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,49 +1,69 @@
-import React from 'react';
+import React from "react";
+// 이미지 가져올시 import 를 통해서 가져와야 한다.
+import watchapedia_logo from "../images/watchapedia_logo.png";
 // import './Header.css';
 // import SearchIcon from "@material-ui/icons/Search"
 // import AccountIcon from "@material-ui/icons/AccountCircle"
-import { useNavigate } from 'react-router-dom';
-import SearchBar from './searchBar';
+import { useNavigate } from "react-router-dom";
+import SearchBar from "./searchBar";
 
 function Header({ isLoggedIn, userObj }) {
   const navigate = useNavigate();
 
   const onLoginClick = () => {
-    navigate('./Login');
+    navigate("./Login");
   };
   const onSignUpClick = () => {
-    navigate('./SignUp');
+    navigate("./SignUp");
   };
   const onProfileClick = () => {
-    navigate('./');
+    navigate("./Profile");
   };
   const onEstimateClick = () => {
-    navigate('./');
+    navigate("./");
   };
 
   return (
     <>
-      <div className='header'>
-        <div className='header_left'>
-          <img className='header_logo' src='img/watchapedia_logo.png' alt='watchapedia_logo' />
-          <div className='header_nav'>
-            <span className='header_menu'>영화</span>
-            <span className='header_menu'>TV</span>
-            <span className='header_menu'>책</span>
-            <span className='header_menu'>웹툰</span>
+      <div className="header">
+        <div className="header_left">
+          {}
+          <img
+            className="header_logo"
+            src={watchapedia_logo}
+            alt="watchapedia_logo"
+          />
+          <div className="header_nav">
+            <span className="header_menu">영화</span>
+            <span className="header_menu">TV</span>
+            <span className="header_menu">책</span>
+            <span className="header_menu">웹툰</span>
           </div>
         </div>
-        <div className='header_right'>
-          <div className='header_option'>
-            <div className='header_search'>
-              <SearchBar className='header_searchInput' isLoggedIn={isLoggedIn} userObj={userObj} />
+        <div className="header_right">
+          <div className="header_option">
+            <div className="header_search">
+              <SearchBar
+                className="header_searchInput"
+                isLoggedIn={isLoggedIn}
+                userObj={userObj}
+              />
             </div>
           </div>
-          {isLoggedIn ? <button onClick={onEstimateClick}>평가하기</button> : <button onClick={onLoginClick}>로그인</button>}
-          {isLoggedIn ? <button onClick={onProfileClick}>프로필 사진</button> : <button onClick={onSignUpClick}>회원가입</button>}
+          {isLoggedIn ? (
+            <button onClick={onEstimateClick}>평가하기</button>
+          ) : (
+            <button onClick={onLoginClick}>로그인</button>
+          )}
+          {isLoggedIn ? (
+            <button onClick={onProfileClick}>프로필 사진</button>
+          ) : (
+            <button onClick={onSignUpClick}>회원가입</button>
+          )}
         </div>
       </div>
     </>
   );
 }
 export default Header;
+// 새로고침하면서 public에 있던왓챠피디아 로고를 늦게 가져오면서 alt값이 출력되는데 import를 통해 바로 가져오면서 해결
--- src/component/router.jsx
+++ src/component/router.jsx
@@ -1,17 +1,23 @@
 // import './Header.css';
 // import SearchIcon from "@material-ui/icons/Search"
 // import AccountIcon from "@material-ui/icons/AccountCircle"
-import Home from '../routes/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
-import React, { useEffect, useState } from 'react';
-import { authService } from '../firebase';
+import Home from "../routes/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { authService } from "../firebase";
 
-import SearchResult from '../routes/searchResult';
-import Login from '../routes/login';
-import SignUp from '../routes/signUp';
-import MovieDetail from '../routes/movieDetail';
-import Header from './Header';
+import SearchResult from "../routes/searchResult";
+import Login from "../routes/login";
+import SignUp from "../routes/signUp";
+import MovieDetail from "../routes/movieDetail";
+import Header from "./Header";
+import Profile from "../routes/profile";
+import ProfileEdit from "../routes/profileEdit";
+import ProfileComment from "../routes/profileComment";
+import ProfileEvaluation from "../routes/profileEvaluation";
+import ProfileCollection from "../routes/profileCollection";
+import NotFound from "../routes/NotFound";
 
 function AppRouter() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -25,6 +31,10 @@ function AppRouter() {
         setIsLoggedIn((loggedIn) => !loggedIn);
         //여기서 user에 유저 정보가 담기고 user.uid로 유저를 특정할 수가 있음
         setUserobj(user);
+      } else {
+        // 로그아웃 상태로 전환
+        setIsLoggedIn((loggedIn) => !loggedIn);
+        setUserobj(user);
       }
     });
   }, []);
@@ -34,7 +44,7 @@ function AppRouter() {
       <Router>
         <Routes>
           <Route
-            path='/'
+            path="/"
             element={
               <>
                 <Header isLoggedIn={isLoggedIn} userObj={userObj} />
@@ -42,10 +52,10 @@ function AppRouter() {
               </>
             }
           />
-          <Route path='/Login' element={<Login isLoggedIn={isLoggedIn} />} />
-          <Route path='/SignUp' element={<SignUp isLoggedIn={isLoggedIn} />} />
+          <Route path="/Login" element={<Login isLoggedIn={isLoggedIn} />} />
+          <Route path="/SignUp" element={<SignUp isLoggedIn={isLoggedIn} />} />
           <Route
-            path='/SearchResult'
+            path="/SearchResult"
             element={
               <>
                 <Header isLoggedIn={isLoggedIn} userObj={userObj} />
@@ -54,7 +64,7 @@ function AppRouter() {
             }
           />
           <Route
-            path='/MovieDetail/:movieSeq'
+            path="/MovieDetail/:movieSeq"
             element={
               <>
                 <Header isLoggedIn={isLoggedIn} userObj={userObj} />
@@ -62,6 +72,52 @@ function AppRouter() {
               </>
             }
           />
+          <Route
+            path="/Profile"
+            element={
+              <>
+                <Header isLoggedIn={isLoggedIn} userObj={userObj} />
+                <Profile isLoggedIn={isLoggedIn} userObj={userObj} />
+              </>
+            }
+          />
+          <Route
+            path="/ProfileEdit"
+            element={
+              <>
+                <Header isLoggedIn={isLoggedIn} userObj={userObj} />
+                <ProfileEdit isLoggedIn={isLoggedIn} userObj={userObj} />
+              </>
+            }
+          />
+          <Route
+            path="/ProfileEvaluation"
+            element={
+              <>
+                <Header isLoggedIn={isLoggedIn} userObj={userObj} />
+                <ProfileEvaluation isLoggedIn={isLoggedIn} userObj={userObj} />
+              </>
+            }
+          />
+          <Route
+            path="/ProfileComment"
+            element={
+              <>
+                <Header isLoggedIn={isLoggedIn} userObj={userObj} />
+                <ProfileComment isLoggedIn={isLoggedIn} userObj={userObj} />
+              </>
+            }
+          />
+          <Route
+            path="/ProfileCollection"
+            element={
+              <>
+                <Header isLoggedIn={isLoggedIn} userObj={userObj} />
+                <ProfileCollection isLoggedIn={isLoggedIn} userObj={userObj} />
+              </>
+            }
+          />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
     </>
@@ -71,3 +127,5 @@ function AppRouter() {
 export default AppRouter;
 
 //Router 태그 안에 있는 컴포넌트만이 useNavigate 훅, Link태그를 사용하여 라우팅이 가능함에 주의한다!
+// * '에스터리스크 ' 를 이용하여 작성하지 않은 Route 예외되는 페이지는 NotFound로 이동
+// useEffect onAuthStateChanged 로그인/로그아웃 상태 잊지말고 구현
