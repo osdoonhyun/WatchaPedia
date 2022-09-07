a8b1472d5a7dde617066f3b7c6c58fd85d0ded16
osdoonhyun <skw1996@naver.com>
2022-09-08 02:44:33 +0900

    Create NotFound page

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+          <h1>페이지가 존재하지 않습니다</h1>
+          <h3>링크를 잘못 입력하셨거나 페이지가 삭제/이동 되었을 수 있습니다.</h3>
+        </div>
+    );
+}
+
+export default NotFound;
